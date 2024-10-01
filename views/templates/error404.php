1a93f8968f936b2b33e2434c83ac23264b4c5da9
wmcsantos <wilsoncavacosantos@gmail.com>
2024-10-01 22:21:22 +0100

    Added 404 error View

---
+++ views/templates/error404.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hackett London | 404</title>
+</head>
+<body>
+    <?php require("header.php") ?>
+    <div class="text-center">
+        <h1 class="status-code-number text-[#525356] font-medium text-[64px] font-serif">404</h1>
+        <h2 class="status-code-title text-[#525356] font-light text-[32px] font-serif tracking-wider">Something isn't quite right.</h2>
+        <p class="status-code-description font-light text-sm">The page you're looking for cannot be found. Not to worry, try to find it by using search or click on the link below:</p>
+        <p class="status-code-description font-light text-sm mt-4 mb-8">Don't give up - check the spelling, or try less specific search terms</p>
+        <a href="/" class="uppercase text-center p-4 hover:bg-[#1f2134] hover:text-white text-xs tracking-wider my-6 bg-white text-[#1f2134] border border-[#1f2134] transition-all duration-300">Return home</a>
+    </div>
+</body>
+</html>
